06910e771cb9ee3abeb2ab8d73cffe39f1187f81
Viktor <malyshkin.viktor@icloud.com>
2020-06-26 17:52:35 +0300

    Update EsLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,36 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
+    root: true,
+    env: {
+        node: true,
+    },
+    extends: [
+        'plugin:vue/essential',
+        '@vue/standard',
+    ],
+    parserOptions: {
+        parser: 'babel-eslint',
+    },
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        indent: ['warn', 4, { MemberExpression: 1 }],
+        'vue/html-indent': ['warn', 4, { baseIndent: 1 }],
+        'vue/script-indent': ['warn', 4, { baseIndent: 1 }],
+        'comma-dangle': ['warn', {
+            arrays: 'always-multiline',
+            objects: 'always-multiline',
+            imports: 'never',
+            exports: 'never',
+            functions: 'never',
+        }],
+        'no-multi-spaces': 'off',
+    },
+    overrides: [
+        {
+            files: ['*.vue'],
+            rules: {
+                indent: 'off',
+            },
+        },
+    ],
 }
